ffec1867a51ac9762f4e230498fce088022aa9f0
himanshugupta09 <hg7984152@gmail.com>
2026-06-29 23:44:04 +0530

    Added Custom URL Feature
    Feature One Done

--- UrlShortner/urlshortner/views.py
+++ UrlShortner/urlshortner/views.py
@@ -1,15 +1,29 @@
+import re
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import URLShortner
-import requests
 
 def home(request):
     if request.method == "POST":
         original_url = request.POST.get("url")
+        custom_alias = request.POST.get("custom_alias","").strip()
+        if custom_alias:
+            if not re.match(r'^[A-Za-z0-9_-]+$', custom_alias):
+                return render(request, "home.html", {
+                    "error": "Alias can only contain letters, numbers, hyphens (-), and underscores (_)."
+                })
+        short_url = None
+        if len(custom_alias) > 0:
+            if URLShortner.objects.filter(short_url=custom_alias).exists():
+                return render(request, "home.html", {
+                    "error": "Custom alias already exists. Please choose a different one."
+                })
+            short_url = custom_alias
 
-        short_url = URLShortner.generate_short_url()
-
-        while URLShortner.objects.filter(short_url=short_url).exists():
+        else:
             short_url = URLShortner.generate_short_url()
+
+            while URLShortner.objects.filter(short_url=short_url).exists():
+                short_url = URLShortner.generate_short_url()
 
         short = URLShortner.objects.create(
             original_url=original_url,
@@ -25,7 +39,7 @@ def home(request):
 
     return render(request, "home.html")
 
-def redirect_to_original(requests, short_url):
+def redirect_to_original(request, short_url):
     url = get_object_or_404(URLShortner, short_url=short_url)
     url.clicks += 1
     url.save()
